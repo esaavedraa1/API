251f1455c76f62250bfd1e473097426a8188cbb0
esaavedraa1 <esaavedra@comseg.cl>
2016-12-05 10:19:41 -0400

    cliente separate app.js

--- app.js
+++ app.js
@@ -4,8 +4,8 @@
  */
 
 var express = require('express');
-//var routes = require('./routes');
-//var user = require('./routes/user');
+var routes = require('./routes');
+var user = require('./routes/cliente.js');
 var http = require('http');
 var path = require('path');
 
@@ -128,89 +128,7 @@ app.post('/Monedas_Tipo/eliminar', function(req, res){
 
 
 //Documentos
-var ClienteSchema = mongoose.Schema({
-	nombre: String,
-    apellido: String,
-    domicilio: String,
-    telefono: String,
-    email: String
-});
-var Cliente = mongoose.model('Cliente', ClienteSchema);
 
-app.get('/', function(req, res){
-	res.sendfile('./public/index.html');
-});
-
-app.get('/listar', function(req, res){
-	Cliente.find({}, function(error, clientes){
-      	if(error){
-      		res.send('Error.');
-      	}else{
-        	res.send(clientes);        	
-      	}
-   })
-});
-
-app.get('/recuperar', function(req, res){
-	Cliente.findById(req.query._id, function(error, documento){
-      	if(error){
-         	res.send('Error.');
-      	}else{
-         	res.send(documento);
-      	}
-   });
-});
-
-app.post('/guardar', function(req, res){
-	if(req.query._id == null){
-		//Inserta
-		var cliente = new Cliente({
-		   nombre: req.query.nombre,
-		   apellido: req.query.apellido,
-		   domicilio: req.query.domicilio,
-		   telefono: req.query.telefono,
-		   email: req.query.email
-		});
-		cliente.save(function(error, documento){
-			if(error){
-			 	res.send('Error.');
-			}else{
-			 	res.send(documento);
-			}
-	   	});
-	}else{
-		//Modifica
-		Cliente.findById(req.query._id, function(error, documento){
-		  	if(error){
-				res.send('Error.');
-		  	}else{
-				var cliente = documento;
-				cliente.nombre = req.query.nombre,
-			   	cliente.apellido = req.query.apellido,
-			   	cliente.domicilio = req.query.domicilio,
-			   	cliente.telefono = req.query.telefono,
-			   	cliente.email = req.query.email
-				cliente.save(function(error, documento){
-			    	if(error){
-			       		res.send('Error.');
-			    	}else{ 
-			       		res.send(documento);
-			    	}
-			 	});
-			}
-		});
-	}
-});
-
-app.post('/eliminar', function(req, res){
-	Cliente.remove({_id: req.query._id}, function(error){
-		if(error){
-			res.send('Error.');
-		}else{
-			res.send('Ok');
-		}
-   });
-});
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
